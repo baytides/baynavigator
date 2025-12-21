d8a9c707a7211e620a0d6b3e5d8d7b1f9e20a6ea
bayareadiscounts <246928973+bayareadiscounts@users.noreply.github.com>
2025-12-20 17:35:33 -0800

    Remove unused Azure Functions and preferences page
    Deleted the SubscribeUser and UnsubscribeUser Azure Functions, which were disabled and not implemented. Also removed the obsolete preferences.md page. Updated Azure Static Web Apps workflow to set app_location to '/' and specify app_artifact_location as '_site'.

--- azure-functions/UnsubscribeUser/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-  context.res = {
-    status: 404,
-    headers: { 'Content-Type': 'application/json' },
-    body: { error: 'Not implemented' }
-  };
-};
